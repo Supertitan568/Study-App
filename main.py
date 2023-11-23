7f00f71694414132479febdf90c0e0c8173901fa
Supertitan568 <laodenbach@gmail.com>
2023-11-22 21:27:37 -0600

    finished the flashcard part of the app

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ front = True
 flashcards = [flashcard("Who is my favorite girl in the world", "My girlfriend"), flashcard("What do I want to do", "Graduate"), flashcard("What is my favorite language", "The C Language")]
 flashcard_index = 0
 
-def keypress(event):
+def next_button_callback():
     global flashcard_index
     global front
     global flashcards
@@ -24,11 +24,32 @@ def keypress(event):
         textbox.config(text=flashcards[flashcard_index].front)
         front = True
 
-window = tk.Tk()
-flashcard_text = flashcards[flashcard_index].front
-textbox = tk.Label(text=flashcards[flashcard_index].front)
-window.bind("<Key>", keypress)
+def prev_button_callback():
+    global flashcard_index
+    global front
+    global flashcards
 
-textbox.pack()
-window.mainloop()
+    if not front:
+        textbox.config(text=flashcards[flashcard_index].front)
+        front= True
+    else:
+        flashcard_index -= 1
+        if flashcard_index >= len(flashcards):
+            exit()
+        textbox.config(text=flashcards[flashcard_index].back)
+        front = False
+
+def main(window):
+    next_button = tk.Button(window, text = "Next", command=next_button_callback)
+    next_button.place(relx = 1, x =-2, y = 2, anchor = tk.NE)
+ 
+    prev_button = tk.Button(window, text = "Previous", command=prev_button_callback)
+    prev_button.place(anchor = tk.NW)
+ 
+    textbox = tk.Label(text=flashcards[flashcard_index].front)
+    textbox.place(relx = 0.5, rely = 0.5, anchor = tk.CENTER)
+
+    window.geometry("600x400")
+
+    window.mainloop()
 
